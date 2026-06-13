f09773822b8f02c1be9d40eaacc1f2808f510149
carol-junko <carol.junko@gmail.com>
2026-06-13 19:08:43 +0200

    add ft_strlen function

---
+++ ft_strlen.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+int	ft_strlen(char	*str)
+{
+	int i;
+	i = 0;
+	while(str[i] != '\0')
+	{
+		i = i + 1;
+	}
+	return i;
+}
+
+int main(void)
+{
+	char *name;
+	char lengt;
+	
+	name = "Carolina";
+	lengt = ft_strlen(name);
+	lengt = lengt + '0';
+	write(1, &lengt, 1);
+	write(1, "\n", 1); 
+	return (0);
+}
+	
+		
+	
