ec5ed1aae930ef34610d0fcf552fa9a365a3ae12
carol-junko <carol.junko@gmail.com>
2026-06-13 18:06:13 +0200

    add strings and null terminator example

---
+++ strings.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+int	main(void)
+{
+	char	name[9];
+	name[0] = 'C';
+	name[1] = 'a';
+	name[2] = 'r';
+	name[3] = 'o';
+	name[4] = 'l';
+	name[5] = 'i';
+	name[6] = 'n';
+	name[7] = 'a';
+	name[8] = '\0';
+	write(1, name, 9);
+	write(1, "\n", 1);
+
+	return (0);
+}
