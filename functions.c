6dd28f35e8bcf689d9fbdbee4d4e9026ada55402
carol-junko <carol.junko@gmail.com>
2026-06-13 16:38:31 +0200

    add print_digit and print_letter functions

---
+++ functions.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+void	print_digit(int n)
+{
+	char c;
+	c = n + '0';
+	write(1, &c, 1);
+	write(1, "\n", 1);
+}
+
+void	print_letter(char d)
+{
+	write(1, &d, 1);
+	write(1, "\n", 1);
+}
+
+int	main(void)
+{
+	print_digit(3);
+	print_digit(7);
+	print_digit(1);
+	
+	print_letter('A');
+        print_letter('B');
+        print_letter('C');
+
+	return (0);
+}
