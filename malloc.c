a729ee94631cfd64d4e1b82c29d79c667969da8a
carol-junko <carol.junko@gmail.com>
2026-06-13 18:34:28 +0200

    add ft_strdup with malloc and free

---
+++ malloc.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char	*dst;
+	int		i;
+
+	dst = malloc(6);
+	if (dst == NULL)
+		return (NULL);
+	i = 0;
+	while (i < 5)
+	{
+		dst[i] = src[i];
+		i = i + 1;
+	}
+	dst[5] = '\0';
+	return (dst);
+}
+
+int	main(void)
+{
+	char	*copy;
+
+	copy = ft_strdup("hello");
+	if (copy == NULL)
+		return (1);
+	write(1, copy, 5);
+	write(1, "\n", 1);
+	free(copy);
+	return (0);
+
+}
