7d68c3b9c9b6977427f89340a3db80d559da9de2
carol-junko <carol.junko@gmail.com>
2026-06-13 16:01:08 +0200

    Program runs loop 0 to 4

---
+++ variables.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+int 	main(void)
+{
+	int	i;
+	char	n;
+	i = 0;
+	while (i < 5)
+	{
+		n = i + '0';
+		write(1, &n, 1);
+		write(1, "\n", 1);
+		i = i + 1;
+	}
+	return (0);
+}
